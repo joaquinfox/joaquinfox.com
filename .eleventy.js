c3b4014d07f311f358686cd7c70454187747bb13
jimmy <joaquin.fox@gmail.com>
2025-05-11 17:09:38 -0700

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  // Enable raw HTML in markdown
+  const markdownIt = require("markdown-it");
+  eleventyConfig.setLibrary("md", markdownIt({ html: true }));
+
+  // Copy assets and CSS directly to output
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("styles.css");
+
+  // Blog collection (sorted newest first)
+  eleventyConfig.addCollection("blog", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("blog/*.md").reverse();
+  });
+
+  // Date formatting filter
+  eleventyConfig.addFilter("date", (value, format = "MMMM d, yyyy") => {
+    return DateTime.fromJSDate(value).toFormat(format);
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "output"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
